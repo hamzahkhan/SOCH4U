955691b2609ff24ce3e2f26a9c2dac5ef039d1b2
Hamzah Khan <khamzah22@gmail.com>
2018-06-01 18:53:53 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="http://soch4u.com/index.html">SOCH4U Home</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="addmember.html">Add new member</a></li>
+      <li><a href="newquery.html">New query</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
