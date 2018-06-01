387086518d83be3630eca6a04b495cbea816fc12
Hamzah Khan <khamzah22@gmail.com>
2018-06-01 18:54:39 +0530

    Update newquery.php

--- newquery.php
+++ newquery.php
@@ -10,28 +10,15 @@
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="http://soch4u.com/index.html">SOCH4U Home</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="addmember.html">Add new member</a></li>
-      <li><a href="newquery.html">New query</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
-  </div>
-</nav>
-
+<?php include_once ("header.php"); ?>
 <div class="container">
 
 <?php
 $servername = "35.185.133.86";
 $username = "aamir";
 $password = "mypass";
+
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
