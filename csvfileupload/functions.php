f8485b794d3701611e28e679d2970d4d9356b9c3
Hamzah Khan <khamzah22@gmail.com>
2018-06-10 03:14:09 +0530

    Update functions.php

--- csvfileupload/functions.php
+++ csvfileupload/functions.php
@@ -5,21 +5,26 @@ $password = "mypass";
 
 $connect = mysqli_connect($servername, $username, $password);
 // echo "Database connected";
-
-$file = fopen("newdetails.csv","r");
-while(! feof($file))
-  {
-  	$getData = fgetcsv($file);
-  	$sql = "INSERT into `SOCH4U`.`new_table` (`name`, `age`, `gender`, `phone`, `blood_group`, `city`) values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."','".$getData[5]."')";
-  	$result = mysqli_query($connect, $sql);
-  }
-  if(!isset($result))
-	{
-		echo 'Invalid file';		
-	}
-	else 
-	{
-		echo 'CSV Uploaded';
-	}
-fclose($file);
+if(isset($_POST["Import"])){
+	$filename=$_FILES["file"]["tmp_name"];
+	if($_FILES["file"]["size"] > 0)
+		{
+			$file = fopen($filename, "r");
+			while(! feof($file))
+			  {
+			  	$getData = fgetcsv($file);
+			  	$sql = "INSERT into `SOCH4U`.`new_table` (`name`, `age`, `gender`, `phone`, `blood_group`, `city`) values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."','".$getData[5]."')";
+			  	$result = mysqli_query($connect, $sql);
+			  }
+			}
+		}
+			  if(!isset($result))
+				{
+					echo 'Invalid file';		
+				}
+				else 
+				{
+					echo 'CSV Uploaded';
+				}
+			fclose($file);
 ?>
